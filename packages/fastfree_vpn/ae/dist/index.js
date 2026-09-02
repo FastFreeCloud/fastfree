7b74118a1261e7bab3a4abd138aff87c22fb4659
FastFree <mohamed.fastfree@gmail.com>
2026-09-02 19:10:06 +0300

    fix: add VPN extension dist/index.js to git

---
+++ packages/fastfree_vpn/ae/dist/index.js
@@ -0,0 +1,18 @@
+/**
+ * Quasar App Extension index/runner script
+ * (runs on each dev/build)
+ *
+ * Docs: https://quasar.dev/app-extensions/development-guide/index-api
+ */
+export default function (api) {
+  api.compatibleWith('quasar', '^2.0.0')
+  api.compatibleWith('@quasar/app-vite', '^3.0.0')
+
+  api.extendQuasarConf(conf => {
+    const boot = (conf.boot ?? [])
+    boot.unshift(
+      '~quasar-app-extension-fastfree_vpn/src/runtime/boot.register.ts'
+    )
+    conf.boot = boot
+  })
+}
