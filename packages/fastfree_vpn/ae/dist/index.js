95e6178d538ecc6c816f142ab2473c1a0148f728
FastFree <mohamed.fastfree@gmail.com>
2026-09-02 19:15:37 +0300

    fix: use exports.default for CJS module

--- packages/fastfree_vpn/ae/dist/index.js
+++ packages/fastfree_vpn/ae/dist/index.js
@@ -4,15 +4,15 @@
  *
  * Docs: https://quasar.dev/app-extensions/development-guide/index-api
  */
-module.exports = function (api) {
-  api.compatibleWith('quasar', '^2.0.0')
-  api.compatibleWith('@quasar/app-vite', '^3.0.0')
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = function (api) {
+  api.compatibleWith('quasar', '^2.0.0');
+  api.compatibleWith('@quasar/app-vite', '^3.0.0');
 
-  api.extendQuasarConf(conf => {
-    const boot = (conf.boot ?? [])
-    boot.unshift(
-      '~quasar-app-extension-fastfree_vpn/src/runtime/boot.register.ts'
-    )
-    conf.boot = boot
-  })
-}
+  api.extendQuasarConf(function (conf) {
+    var _a;
+    var boot = (_a = conf.boot) !== null && _a !== void 0 ? _a : [];
+    boot.unshift('~quasar-app-extension-fastfree_vpn/src/runtime/boot.register.ts');
+    conf.boot = boot;
+  });
+};
